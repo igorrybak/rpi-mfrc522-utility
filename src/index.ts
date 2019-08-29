52fb1682d833fc2a26f95c4c26765cb05f43c5d7
Igor <zathershtein@gmail.com>
2019-08-29 18:18:22 +0300

    Check cli arg

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import * as RpiMfrc522 from "rpi-mfrc522";
 import { config } from "./config";
 
-export default class MFRC {
-    protected settings = {
-        resetGPIO: config.PIN_RESET,
-        resetTime: config.RESET_TIME,
-        spiBus: config.BUS,
-        spiDevice: config.DEVICE,
-    };
+const settings = {
+    resetGPIO: config.PIN_RESET,
+    resetTime: config.RESET_TIME,
+    spiBus: config.BUS,
+    spiDevice: config.DEVICE,
+};
 
-    mfrc522 = new RpiMfrc522(this.settings);
+const mfrc522 = new RpiMfrc522(settings);
+const blockNumber = parseInt(process.argv[2]);
+
+if (process.argv.length > 3 || !blockNumber) {
+    console.error("You must specify only one numeric argument");
+    process.exit(1);
 }
+
+console.info("Using KEY: ", config.KEY_A);
+console.info("Reading BLOCK: ", blockNumber);
+console.info("Write DATA: ", config.NEW_DATA_BLOCK);
