7acbf310e62301b136839f4d07b9e82a4addb206
Igor <zathershtein@gmail.com>
2019-09-01 13:52:59 +0300

    Package.json added

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,12 @@ const mfrc522 = new RpiMfrc522(settings);
 const blockNumber = parseInt(process.argv[2]);
 
 if (process.argv.length > 3 || !blockNumber) {
-    console.error("You must specify only one numeric argument");
+    console.error("========== You must specify only one numeric argument! ==========");
     process.exit(1);
 }
 
-console.info("Using KEY: ", config.KEY_A);
-console.info("Reading BLOCK: ", blockNumber);
-console.info("Write DATA: ", config.NEW_DATA_BLOCK);
+console.info("\n\n----------Starting utility--------------");
+console.info("Using KEY:\t", config.KEY_A);
+console.info("Reading BLOCK:\t", blockNumber);
+console.info("Write DATA:\t", config.NEW_DATA_BLOCK);
+console.info("----------Finished----------\n\n");
